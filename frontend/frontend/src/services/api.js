d3dc0522e1a268efc61b1c9b23e69cb6b6c6c592
Siddhant Jain <siddhant1811@gmail.com>
2026-05-18 03:18:55 +0530

    Updated production API URL

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mern-team-task-manager.onrender.com",
 });
 
 export default API;
